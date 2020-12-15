5463301d57e8a38a34a09a2ee852aeae6c20c961
Zahra-Kadkhodaie <zk388@nyu.edu>
2020-12-15 12:57:18 -0500

    Add files via upload

--- code/Utils_inverse_prob.py
+++ code/Utils_inverse_prob.py
@@ -15,9 +15,9 @@ def single_image_loader(data_set_dire_path, image_number):
     all_names = os.listdir(data_set_dire_path)
     file_name = all_names[image_number]
     im = plt.imread(data_set_dire_path + file_name)
-    if len(im.shape) == 3: 
+    if len(im.shape) == 3:
         x = torch.tensor(im).permute(2,0,1)
-    elif len(im.shape) == 2: 
+    elif len(im.shape) == 2:
         x = torch.tensor(im.reshape(1, im.shape[0], im.shape[1]))
     return x
 
@@ -30,55 +30,54 @@ def plot_synthesis(intermed_Ys, sample):
     f, axs = plt.subplots(1,len(intermed_Ys), figsize = ( 4*len(intermed_Ys),4))
     axs = axs.ravel()
 
-    for ax in range(len(intermed_Ys)): 
+    for ax in range(len(intermed_Ys)):
         x = intermed_Ys[ax].permute(1,2,0).detach().numpy()
-        if x.shape[2] == 1: 
+        if x.shape[2] == 1:
             fig = axs[ax].imshow(rescale_image(x[:,:,0]), 'gray')
-        else: 
+        else:
             fig = axs[ax].imshow(rescale_image(x))
         axs[ax].axis('off')
-    
+
     sample = sample.permute(1,2,0).detach().numpy()
-    if sample.shape[2] == 1:  
+    if sample.shape[2] == 1:
         fig = axs[-1].imshow(rescale_image(sample[:,:,0]),'gray' )
-    else: 
+    else:
         fig = axs[-1].imshow(rescale_image(sample))
 
     axs[-1].axis('off')
-    print('value range', np.round(np.min(sample ),2), np.round(np.max(sample),2) )  
-    
+    print('value range', np.round(np.min(sample ),2), np.round(np.max(sample),2) )
 
 def plot_sample(x, corrupted, sample):
     f, axs = plt.subplots(1,3, figsize = (15,5))
     axs = axs.ravel()
     x = x.permute(1,2,0)
-    if x.shape[2] == 1:  
+    if x.shape[2] == 1:
         fig = axs[0].imshow( x[:,:,0], 'gray', vmin=0, vmax = 1)
-    else: 
+    else:
         fig = axs[0].imshow( x, vmin=0, vmax = 1)
 
     axs[0].axis('off')
     axs[0].set_title('clean')
-    
+
     corrupted = corrupted.permute(1,2,0)
-    if corrupted.shape[2] == 1:      
+    if corrupted.shape[2] == 1:
         fig = axs[1].imshow(corrupted[:,:,0], 'gray',vmin=0, vmax = 1)
-    else: 
+    else:
         fig = axs[1].imshow( corrupted, vmin=0, vmax = 1)
-        
+
     axs[1].axis('off')
     axs[1].set_title('measured')
     axs[1].set_title('measured \n psnr: '+str(np.round(peak_signal_noise_ratio(x.permute(1,2,0).numpy(), corrupted.permute(1,2,0).numpy() ))))
 
     sample = sample.detach().permute(1,2,0)
-    if sample.shape[2] == 1:  
+    if sample.shape[2] == 1:
         fig = axs[2].imshow(sample[:,:,0],'gray' ,vmin=0, vmax = 1)
         ssim = np.round(structural_similarity(x[:,:,0].numpy(), sample[:,:,0].numpy()  ) ,3 )
-        
-    else: 
+
+    else:
         fig = axs[2].imshow(sample,vmin=0, vmax = 1)
         ssim = np.round(structural_similarity(x.numpy(), sample.numpy() , multichannel=True) ,3)
-        
+
     axs[2].axis('off')
     axs[2].set_title('reconstructed')
     psnr = np.round(peak_signal_noise_ratio(x.numpy() ,sample.numpy() ),2)
@@ -94,24 +93,24 @@ def plot_all_samples(sample, intermed_Ys):
 
     for ax in range(len(intermed_Ys)):
         x = intermed_Ys[ax].detach().permute(1,2,0)
-        if x.shape[2] == 1: 
+        if x.shape[2] == 1:
             fig = axs[ax].imshow(x[:,:,0], 'gray')
-        else: 
-            fig = axs[ax].imshow(x)
+        else:
+            fig = axs[ax].imshow(rescale_image(x))
         axs[ax].axis('off')
-    
+
     sample = sample.detach().permute(1,2,0)
-    if sample.shape[2] == 1:  
+    if sample.shape[2] == 1:
         fig = axs[-1].imshow(sample[:,:,0],'gray' )
-    else: 
-        fig = axs[-1].imshow(sample)
+    else:
+        fig = axs[-1].imshow(rescale_image(sample))
     axs[-1].axis('off')
     plt.colorbar(fig, ax=axs[-1], fraction=.05)
 
 
     for ax in range(len(intermed_Ys),n_rows*4 ):
         axs[ax].axis('off')
-        
+
 ###################################### Inverse problems Tasks ##################################
 #############################################################################################
 class synthesis:
@@ -212,13 +211,13 @@ class random_basis:
         self.U, _ = torch.qr(torch.randn(int(np.prod(x_size)),int(np.prod(x_size)*p) ))
         if torch.cuda.is_available():
             self.U = self.U.cuda()
-            
+
     def M_T(self, x):
         # gets 2d or 3d image and returns flatten partial measurement(1d)
         return torch.matmul(self.U.T,x.flatten())
 
     def M(self, x):
-        # gets flatten partial measurement (1d), and returns 2d or 3d reconstruction 
+        # gets flatten partial measurement (1d), and returns 2d or 3d reconstruction
         return torch.matmul(self.U,x).reshape(self.x_size[0], self.x_size[1], self.x_size[2])
 
 #############################################################################################
@@ -228,7 +227,7 @@ class random_basis:
 #### fix: only for images of odd dims. extend!
 class spectral_super_resolution:
     '''
-    creates a mask for dropping high frequency coefficients 
+    creates a mask for dropping high frequency coefficients
     @im_d: dimension of the input image is (im_d, im_d)
     @p: portion of coefficients to keep
     '''
@@ -244,14 +243,14 @@ class spectral_super_resolution:
         mask[f0+1:x_size[0]-f0,:]=0
         mask[:, f1+1:x_size[1]-f1]=0
         self.mask = mask
-        
+
 
     def M_T(self, x):
         return self.mask*torch.fft.fftn(x, norm= 'ortho', s = self.x_size)
 
     def M(self, x):
         return torch.real(torch.fft.ifftn(x, norm= 'ortho', s = self.x_size))
-    
+
 
 
 
